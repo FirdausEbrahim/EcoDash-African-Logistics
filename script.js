2c9c9ba109216bf41d3a111a496757d08c0b1121
FirdausEbrahim <25302156@stadio.ac.za>
2026-09-18 23:33:53 +0200

    Adding basic keyboard player movement

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight - 140;
 
+let playerX = 100;
+let playerY = canvas.height / 2;
+let playerSpeed = 5;
+
 function drawBackground() {
     ctx.fillStyle = "#d6b87c";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -12,8 +16,28 @@ function drawBackground() {
 
 function drawPlayer() {
     ctx.fillStyle = "green";
-    ctx.fillRect(100, canvas.height / 2, 60, 35);
+    ctx.fillRect(playerX, playerY, 60, 35);
 }
+
+document.addEventListener("keydown", function(event) {
+
+    if (event.key == "ArrowRight") {
+        playerX += playerSpeed;
+    }
+
+    if (event.key == "ArrowLeft") {
+        playerX -= playerSpeed;
+    }
+
+    if (event.key == "ArrowUp") {
+        playerY -= playerSpeed;
+    }
+
+    if (event.key == "ArrowDown") {
+        playerY += playerSpeed
+    }
+});
+
 
 function animate() {
 ctx.clearRect(0, 0, canvas.width, canvas.height);
