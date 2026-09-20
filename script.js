133762420bf35c59221a3bf51d420a88724973d5
FirdausEbrahim <25302156@stadio.ac.za>
2026-09-20 21:16:59 +0200

    Adding velocity acceleration and friction

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ canvas.height = window.innerHeight - 140;
 
 let playerX = 100;
 let playerY = canvas.height / 2;
-let playerSpeed = 5;
+
+let velocityX = 0;
+let velocityY = 0;
+
+let acceleration = 0.3; 
+let friction = 0.95;
+let maxSpeed = 6;
+
+let keys = {};
 
 function drawBackground() {
     ctx.fillStyle = "#d6b87c";
@@ -20,30 +28,67 @@ function drawPlayer() {
 }
 
 document.addEventListener("keydown", function(event) {
-
-    if (event.key == "ArrowRight") {
-        playerX += playerSpeed;
-    }
-
-    if (event.key == "ArrowLeft") {
-        playerX -= playerSpeed;
-    }
-
-    if (event.key == "ArrowUp") {
-        playerY -= playerSpeed;
-    }
-
-    if (event.key == "ArrowDown") {
-        playerY += playerSpeed
-    }
+    keys[event.key] = true;
 });
 
+document.addEventListener("keyup", function(event) {
+    keys[event.key] = false;
+});
+
+function movePlayer() {
+
+
+ if (keys["ArrowRight"]) {
+        velocityX += acceleration;
+    }
+
+    if (keys["ArrowLeft"]) {
+        velocityX -= acceleration;
+    }
+
+    if (keys["ArrowUp"]) {
+      velocityY -= acceleration;
+    }
+
+   if (keys["ArrowDown"]) {
+      velocityY += acceleration;
+   }
+
+
+velocityX *= friction;
+velocityY *= friction;
+
+if (velocityX > maxSpeed) {
+    velocityX = maxSpeed;
+
+}
+
+if (velocityX < -maxSpeed) {
+    velocityX = -maxSpeed;
+    
+}
+
+if (velocityY > maxSpeed) {
+    velocityY = maxSpeed;
+    
+}
+
+if (velocityY < -maxSpeed) {
+    velocityY = -maxSpeed;
+    
+}
+
+playerX += velocityX;
+playerY += velocityY;
+
+}
 
 function animate() {
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 drawBackground();
 drawPlayer();
+movePlayer();
 
 requestAnimationFrame(animate);
 
